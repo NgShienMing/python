a0bcc21a0fe93763b0a697f0a00cf596a72e1d40
ngshienming <nsming1030@gmail.com>
2021-12-12 01:59:06 +0800

    done 1003 q4 class test

--- From 1003/Week 4/q4_class_test.py
+++ From 1003/Week 4/q4_class_test.py
@@ -1,5 +1,7 @@
 """Drawing shapes by printing *"""
-def cross(size: int):
+import math
+
+def draw_cross(size: int):
     """Draw a cross"""
     arr = ["*"]*size
     front = 0
@@ -16,4 +18,45 @@ def cross(size: int):
         front += 1
         back -= 1
     print(output)
-cross(7)
+
+def draw_plus(size: int):
+    """Draw a plus"""
+    output = ""
+    for i in range(1, size+1):
+        if not i == math.ceil(size/2):
+            output += " "*math.floor(size/2) + "*" + " "*math.floor(size/2) + "\n"
+        else:
+            output += "*"*size + "\n"
+    print(output)
+
+def draw_rectangle(width, height):
+    """Draw a rectangle"""
+    output = ""
+    for i in range(1, height+1):
+        if i in (1, height):
+            output += "*"*width + "\n"
+        else:
+            output += "*" + " "*(width-2) + "*" + "\n"
+    print(output)
+
+def draw_z(size):
+    """Draw a Z"""
+    output = ""
+    for row in range(size+1):
+        for col in range(size):
+            if row in (0, size):
+                output += "*"
+            elif (row+col) == size:
+                output += "*"
+            else:
+                output += " "
+        output += "\n"
+    print(output)
+
+SIZE = 5
+WIDTH = 5
+HEIGHT = 6
+draw_cross(SIZE)
+draw_plus(SIZE)
+draw_rectangle(WIDTH, HEIGHT)
+draw_z(SIZE)

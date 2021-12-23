73f9e28c48e90ac33c32f2143d7f4750bd1f7a6c
ngshienming <nsming1030@gmail.com>
2021-12-23 15:00:26 +0800

    count

--- General Learning/main.py
+++ General Learning/main.py
@@ -3,8 +3,19 @@
 import people as ppl
 import day
 
+def total(*num, initial = 5, **key):
+    """Counter"""
+    count = initial
+    for number in num:
+        count += number
+    for k in key:
+        count += key[k]
+    return count
+
 nsm = ppl.Person("Ng Shien Ming", "June 2")
 nsm.to_string()
 
 day.calc_day_in_year(2002, 6, 2)
+
+print(total(100, 2, 3, clouds = 50, stars = 100))
 print(__name__)
